927c2da23cafa9ca63f763ac849ccd6f1965da4b
chrizteroo <108843788+chrizteroo@users.noreply.github.com>
2023-10-30 14:22:00 -0400

    Added Functions to change images

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useRef } from 'react';
 
 const App = () => {
-    const svgContainerRef = useRef();
+  const svgContainerRef = useRef();
 
   useEffect(() => {
-    // Fetch the SVG from a file and load it into the div with id="svg-container"
-    fetch('/assets/My_License2.svg')
+    // Fetch the SVG from the public folder and inject it into the div
+    fetch('assets/My_License2.svg')  
       .then(response => response.text())
       .then(svgString => {
         svgContainerRef.current.innerHTML = svgString;
@@ -14,33 +14,47 @@ const App = () => {
         const svgElement = svgContainerRef.current.querySelector('svg');
 
         // Function to update SVG text based on id
-         const updateTextById = (id, newText) => {
-            const element = svgElement.getElementById(id);
-            if (element) element.textContent = newText;
-          };
-          //Update the SVG text Fields
-          updateTextById('textFNLN', 'DOW JOHN');
-          updateTextById('textStreetAddress', '1273 TALL PINE AVE OSHAWA ON, L2V 1T6');
-          updateTextById('textNumber', 'D 1023-7633-82292');
-          updateTextById('textExpiryON', '2029/01/23');
-          updateTextById('textIssueDate', '2023/10/29');
-          updateTextById('ReferenceNumber', 'D912341003');
-          updateTextById('textHeight', '165 cm');
-          updateTextById('textGender', 'M');
-          updateTextById('textCategory', 'G1');
-          updateTextById('textDOB', '1999/10/29');
-        })
-        .catch(error => {
-            console.error("Error fetching SVG:", error);
-        });
-        },[] );
+        const updateTextById = (id, newText) => {
+          const element = svgElement.getElementById(id);
+          if (element) element.textContent = newText;
+        };
 
-        return(
-            <div ref={svgContainerRef}>
-                {/* SVG will be injected here */}
-            </div>
-        );
-    };   
-    export default App;
+// Update the SVG text fields
+        updateTextById('textFNLN', 'DIMEJI, CHRISTOPHER');
+        updateTextById('textStreetAddress', '34 KIPLING PINE AVE ETOBICOKE ON, L2V 1T6');
+        updateTextById('textNumero', 'D 1023-7633-82292');
+        updateTextById('textExpiryDate', '2029/01/23');
+        updateTextById('textIssueDate', '2023/10/29');
+        updateTextById('textReferenceNumber', 'D912341003');
+        updateTextById('textHeight' , '186 cm');
+        updateTextById('textGender', 'M');
+        updateTextById('textCategory', 'G2');
+        updateTextById('textDOB', '1999/10/29');
 
 
+        // Function to update image based on id
+      /*const updateImageById = (id, newImageSrc) => {
+          const imageElement = svgElement.getElementById(id);
+          if (imageElement) {
+            // Using 'xlink:href' since 'href' might not work for embedded SVGs
+            imageElement.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', newImageSrc);
+          }
+        };
+
+        // Update the SVG image
+        // The new image is 'manImage.jpg' and is located in the Public folder
+        updateImageById('ladyImage', '/manImage.jpg');
+      */ })
+      .catch(error => {
+        console.error("Error fetching SVG:", error);
+      });
+  }, []);
+
+  return (
+    <div ref={svgContainerRef}>
+      {/* SVG will be injected here */}
+    </div>
+  );
+};
+
+export default App;
